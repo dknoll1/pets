019e5c4d140e3fd7037c6050acee9a13b2511b06
Daniel Knoll <knollmdan@gmail.com>
2024-01-18 12:16:30 -0500

    added index page'

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+/* Daniel Knoll
+ * January 2024
+ * https://dknoll.greenriverdev.com/328/pets/
+ * this is my controller for the pets app
+ */
+
+// turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// require the autoload file
+require_once ('vendor/autoload.php');
+
+// instantiate fat-free framework (f3)
+$f3 = Base::instance();
+
+// Define a default route
+$f3->route('GET /', function() {
+    echo "Pet Home";
+    //echo "My Diner";
+    //display a view page
+//    $view = new Template();
+//    echo $view->render('views/home.html');
+});
+
+$f3->run();
